4e084889dc1405a0f881ccf6bd5ef8b9c9333f54
tejaschaudhari131 <you@example.com>
2025-12-01 18:21:56 +0530

    fix: Remove strict type check from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
